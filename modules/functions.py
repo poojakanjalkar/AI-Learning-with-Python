b53363f53542229781dc1ada60ae81322969a5fd
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-12-10 19:34:42 +0530

    created own module

---
+++ modules/functions.py
@@ -0,0 +1,10 @@
+
+
+def calculate_area_of_rectangle(length,breadth):
+  area=length*breadth
+  return area
+  
+  pi=3.14
+  def calculate_area_of_circle(pi,r,r):
+    circle_area=pi*r*r
+    return circle_area
---
+++ modules/myprogram.py
@@ -0,0 +1,8 @@
+import functions as f
+area=f.calculate_area_of_rectangle(2,2)
+print(area)
+
+
+area2=f.calculate_area_of_circle(3.14,4,4)
+print(area2)
+
