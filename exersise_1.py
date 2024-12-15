7ca4170779f66566c816e2c7f24551dd186b5509
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-12-15 21:22:26 +0530

    updated new code

--- exersise_1.py
+++ exersise_1.py
@@ -1,3 +1,21 @@
-f=open("./input.txt","w")
-f.write("6,7\n7,8\n2,8\n9,5\n9,6")
-f.close()
+# f=open("./input.txt","w")
+# f.write("6,7\n7,8\n2,8\n9,5\n9,6")
+def sumOfNumber():
+  
+  with open("./input.txt","r") as f:
+    # print(f.read())
+
+    for line in f:
+      numbers=[int(x)
+               for x in
+               line.split(",")]
+      total=sum(numbers)
+      return total
+      # for n in numbers:
+      # # print(n)
+    #     sum=n[0]+n[1]
+    # return sum
+    
+print("sum:",sumOfNumber())
+# f.close()
+# f_out.close()
