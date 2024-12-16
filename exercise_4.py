7b3c090c9c545b190191ef7d226d6c067fbbc7ee
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-12-16 17:52:19 +0530

    class and object

---
+++ exercise_4.py
@@ -0,0 +1,22 @@
+class Human:
+  def __init__(self,n,o):
+    self.name=n
+    self.occupation=o
+    
+  def player(self):
+      if(self.name=="jack"):
+        print("jack is tennis player")
+        
+      elif(self.name=="rom"):
+        print("rom is cricketer")
+        
+  def work_do(self):
+      if(self.occupation=="tennis"):
+        print(self.name,"says hello")
+      
+      elif(self.occupation=="cricketer"):
+        print(self.name,"says hello")
+      
+h=Human("jack","tennis")
+h.player()
+h.work_do()
